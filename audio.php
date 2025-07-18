e3a231906e3dabebb93d221cb1e4240897c34a3d
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-07-18 15:07:56 +0200

    update audios

--- audio.php
+++ audio.php
@@ -24,18 +24,26 @@ $link = "index.php?audioszoba=" . $lang;
 
 <?php if (isset($_POST['angol_01'])) { ?>
 	<div class="audio-grid">
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/12Bog9R49ok54xAxBBNjAS92FKAuQ3rIk/view?usp=drive_link','_blank');return false;">K01</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1zoq7dFpFFr-8MuMEo3hsKKBd-7jTPi8b/view?usp=sharing','_blank');return false;">K02</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1VHxgabMvldA7qziTzFhEomu4yMoaJXzQ/view?usp=sharing','_blank');return false;">K03</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1ROm8ccULkrMBJptD7bgPwgzfr1syTF7P/view?usp=sharing','_blank');return false;">K04</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1WuozLWE6yRbEUXLWBEepo4aY28EW6rtD/view?usp=sharing','_blank');return false;">K05</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1Of-2pn-n5bsz75XGPd1vpor7qG58gDvx/view?usp=sharing','_blank');return false;">K06</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1VS1I5rHooC-m692SmJLWRefvuIy7nXtW/view?usp=sharing','_blank');return false;">K07</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1odlZixvQVZar9CSnM3Dmw2J9rjtuZ2Zf/view?usp=sharing','_blank');return false;">K08</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1JdIpJi1c-D9-mIeTOkDmjq3keOSXgxQ-/view?usp=sharing','_blank');return false;">K09</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/16HKNtpA60JBB_LFbUAm8-34ThlX8mWHV/view?usp=sharing','_blank');return false;">K10</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1yF5qA0wNe2Zj371a_3mc9ZsXuru9fgSf/view?usp=sharing','_blank');return false;">K11</button>
-		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1m5Amq2eFZs68BhweSBssUGYr__RHDM_l/view?usp=sharing','_blank');return false;">K12</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/12Bog9R49ok54xAxBBNjAS92FKAuQ3rIk/view?usp=drive_link','_blank');return false;">1</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1zoq7dFpFFr-8MuMEo3hsKKBd-7jTPi8b/view?usp=sharing','_blank');return false;">2</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1VHxgabMvldA7qziTzFhEomu4yMoaJXzQ/view?usp=sharing','_blank');return false;">3</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1ROm8ccULkrMBJptD7bgPwgzfr1syTF7P/view?usp=sharing','_blank');return false;">4</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1WuozLWE6yRbEUXLWBEepo4aY28EW6rtD/view?usp=sharing','_blank');return false;">5</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1Of-2pn-n5bsz75XGPd1vpor7qG58gDvx/view?usp=sharing','_blank');return false;">6</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1VS1I5rHooC-m692SmJLWRefvuIy7nXtW/view?usp=sharing','_blank');return false;">7</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1odlZixvQVZar9CSnM3Dmw2J9rjtuZ2Zf/view?usp=sharing','_blank');return false;">8</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1JdIpJi1c-D9-mIeTOkDmjq3keOSXgxQ-/view?usp=sharing','_blank');return false;">9</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/16HKNtpA60JBB_LFbUAm8-34ThlX8mWHV/view?usp=sharing','_blank');return false;">10</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1yF5qA0wNe2Zj371a_3mc9ZsXuru9fgSf/view?usp=sharing','_blank');return false;">11</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1m5Amq2eFZs68BhweSBssUGYr__RHDM_l/view?usp=sharing','_blank');return false;">12</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1Q9OqXAgfx0biN06ERxDDcL9GIkyMTf3r/view?usp=sharing','_blank');return false;">13</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1cvRafh94KPkoJRYyRGLLE_pBrFJfaM67/view?usp=sharing','_blank');return false;">14</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1qneYZZ0h0XNxxVHtvDIan_EGc79Zavyv/view?usp=sharing','_blank');return false;">15</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1mFBfa9ZnTsO8wzL8n_frnGXQ1Q6iG3F9/view?usp=sharing','_blank');return false;">16</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1monhMXDZzv-ZsCZsxa1f6efCF6bgeegd/view?usp=sharing','_blank');return false;">17</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1VXeRjZl5F3ETkoPMSWEsaT81Vq-Q_3hV/view?usp=sharing','_blank');return false;">18</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1292D5hBczKQaZD3euq2qCakVUFjwtTbx/view?usp=sharing','_blank');return false;">19</button>
+		<button class="audio-btn" onclick="window.open('https://drive.google.com/file/d/1evNi84i_s7sgURm5v08PL8GWp3h1pIls/view?usp=sharing','_blank');return false;">20</button>
 	</div>
 <?php } ?>
 
