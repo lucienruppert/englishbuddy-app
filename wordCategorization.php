129688f57d4d647e044687bbad20c17adeccecd1
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-07-18 07:56:53 +0200

    fix warnings 3

--- wordCategorization.php
+++ wordCategorization.php
@@ -1,44 +1,50 @@
 <?php
 global $_SESSION;
 
-if(!$userObject){
+if (!$userObject) {
     include_once('index.php');
     exit;
 }
 
+
 $ext = getLangExtByLangId($userObject['nyelv']);
 $forras_nyelv_ext = getLangExtByLangId($GLOBALS['userObject']['forras_nyelv']);
 
-if(!is_array($_SESSION['wordCategorization']) || $_REQUEST['source'] == 'welcome'){
+if (!is_array($_SESSION['wordCategorization']) || (isset($_REQUEST['source']) && $_REQUEST['source'] == 'welcome')) {
     $_SESSION['wordCategorization'] = getNotCategorizedWordIds();
 }
-if($_REQUEST['store'] == 1){
-    $selectedId = $_REQUEST['selectedId'];
-    $category = $_REQUEST['category'];
+if (isset($_REQUEST['store']) && $_REQUEST['store'] == 1) {
+    $selectedId = isset($_REQUEST['selectedId']) ? $_REQUEST['selectedId'] : null;
+    $category = isset($_REQUEST['category']) ? $_REQUEST['category'] : null;
 
-    if(!setWordCategory($selectedId, $category)){
-        print "<script>alert('R�gz�t�s nem siker�lt!')</script>";
+    if ($selectedId !== null && $category !== null) {
+        if (!setWordCategory($selectedId, $category)) {
+            print "<script>alert('Rögzítés nem sikerült!')</script>";
+        }
     }
 }
 
-$wordCount = count($_SESSION['wordCategorization']);
-$currentWordId = array_shift($_SESSION['wordCategorization']);
-$currentWord = getWordById($currentWordId);
 
-$wordOrig = $currentWord['word_' . $forras_nyelv_ext];
-$wordForeign = $currentWord['word_' . $ext];
+$wordCount = is_array($_SESSION['wordCategorization']) ? count($_SESSION['wordCategorization']) : 0;
+$currentWordId = ($wordCount > 0) ? array_shift($_SESSION['wordCategorization']) : null;
+$currentWord = ($currentWordId !== null) ? getWordById($currentWordId) : null;
+
+$wordOrig = ($currentWord && isset($currentWord['word_' . $forras_nyelv_ext])) ? $currentWord['word_' . $forras_nyelv_ext] : '';
+$wordForeign = ($currentWord && isset($currentWord['word_' . $ext])) ? $currentWord['word_' . $ext] : '';
 
 ?>
 
 <style>
-    .btnCategory{
+    .btnCategory {
         height: 100px;
         width: 200px;
     }
 </style>
 
 <div name='quickLearning' id='quickLearning'>
-    <table cellspacing=0 cellpadding=0><tr><td width='50' align='left' valign='middle' style='background-color:grey;'>
+    <table cellspacing=0 cellpadding=0>
+        <tr>
+            <td width='50' align='left' valign='middle' style='background-color:grey;'>
         <tr>
             <td align='center' height='50' valign='center' style='width:100%;font-size:20pt;color:white;background-color:<?php print $colorValue; ?>'>Kategoriz�l�s</td>
         </tr>
@@ -63,15 +69,13 @@ $wordForeign = $currentWord['word_' . $ext];
     </table>
 </div>
 <script>
+    function store(category) {
+        var selectedId = <?php print $currentWordId; ?>;
+        location.href = 'main.php?content=wordCategorize&store=1&selectedId=' + selectedId + '&category=' + category;
+    }
 
-function store(category){
-    var selectedId = <?php print $currentWordId; ?>;
-    location.href = 'main.php?content=wordCategorize&store=1&selectedId=' + selectedId + '&category=' + category;
-}
-
-function doOnClickBody() {
-    location.href='main.php?content=wordCategorize&inbetween=1';
-}
-document.onclick = doOnClickBody;
-
+    function doOnClickBody() {
+        location.href = 'main.php?content=wordCategorize&inbetween=1';
+    }
+    document.onclick = doOnClickBody;
 </script>
