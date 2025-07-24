e3f4ac3bbfa31b4e16f152fa18c503bcf1a20c15
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-07-24 14:29:18 +0200

    solve encoding

--- mainDiv.php
+++ mainDiv.php
@@ -1,94 +1,102 @@
-<?php include_once('login.php'); ?>
-<div id='mainDiv' style=<?php print "' . $mainStyleText . '"; ?>>
-  <?php if ($userObject) { ?>
-    <div class="navigation-mainDiv">
-      <span class="welcome hello">
-        <?php
-        if ($userObject)
-          print translate('szia') . "&nbsp;" . $userObject['keresztnev'] . "!";
-        else
-          print "";
-        ?>
-      </span>
-      <span class='hamburger'><img src='images/hamburger.svg' width="30px" height="30px" onclick="toggleMenu();"></span>
-      <span style="display:<?php echo $_SESSION['isShown'] ? 'none' : 'flex'; ?>" class="welcome submenu">
-        <div class="dropdown" style="display:inline-block;">
-          <a href="#" class="medium-color" onclick="event.preventDefault();document.getElementById('szogyakorlas-submenu').classList.toggle('show');">Szógyakorlás &#9662;</a>
-          <div id="szogyakorlas-submenu" class="dropdown-content" style="display:none;position:absolute;z-index:100;background:#222;padding:5px 0;border-radius:6px;min-width:160px;">
-            <a href="#" class="medium-color" onclick="sajatSzavak();">Saját szótár</a>
-            <a href="#" class="medium-color" onclick="alapszokincs();">Alap szókincs</a>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+</head>
+
+<body>
+  <?php include_once('login.php'); ?>
+  <div id='mainDiv' style=<?php print "' . $mainStyleText . '"; ?>>
+    <?php if ($userObject) { ?>
+      <div class="navigation-mainDiv">
+        <span class="welcome hello">
+          <?php
+          if ($userObject)
+            print translate('szia') . "&nbsp;" . $userObject['keresztnev'] . "!";
+          else
+            print "";
+          ?>
+        </span>
+        <span class='hamburger'><img src='images/hamburger.svg' width="30px" height="30px" onclick="toggleMenu();"></span>
+        <span style="display:<?php echo $_SESSION['isShown'] ? 'none' : 'flex'; ?>" class="welcome submenu">
+          <div class="dropdown" style="display:inline-block;">
+            <a href="#" class="medium-color" onclick="event.preventDefault();document.getElementById('szogyakorlas-submenu').classList.toggle('show');">Szógyakorlás &#9662;</a>
+            <div id="szogyakorlas-submenu" class="dropdown-content" style="display:none;position:absolute;z-index:100;background:#222;padding:5px 0;border-radius:6px;min-width:160px;">
+              <a href="#" class="medium-color" onclick="sajatSzavak();">Saját szótár</a>
+              <a href="#" class="medium-color" onclick="alapszokincs();">Alap szókincs</a>
+            </div>
           </div>
-        </div>
-        <a href='#' class="medium-color" onclick="peldamondatok();"><? print translate("tudastar"); ?></a>
-        <a href='#' class="medium-color show-menu" onclick="intelligensGyakorlo()"><? print translate("intelligensgyakorlo"); ?></a>
-        <style>
-          .dropdown-content a {
-            display: block;
-            color: white;
-            padding: 8px 16px;
-            text-decoration: none;
-          }
-
-          .dropdown-content a:hover {
-            background: #444;
-          }
-
-          .dropdown .medium-color {
-            cursor: pointer;
-          }
-
-          .dropdown-content {
-            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-          }
-
-          .dropdown .show {
-            display: block !important;
-          }
-        </style>
-        <script>
-          document.addEventListener('click', function(e) {
-            var submenu = document.getElementById('szogyakorlas-submenu');
-            if (submenu && !e.target.closest('.dropdown')) {
-              submenu.classList.remove('show');
+          <a href='#' class="medium-color" onclick="peldamondatok();"><? print translate("tudastar"); ?></a>
+          <a href='#' class="medium-color show-menu" onclick="intelligensGyakorlo()"><? print translate("intelligensgyakorlo"); ?></a>
+          <style>
+            .dropdown-content a {
+              display: block;
+              color: white;
+              padding: 8px 16px;
+              text-decoration: none;
             }
-          });
-        </script>
-      <?php } ?>
-      <?php if ($userObject && !in_array($userObject["status"], array(1, 2))) { ?>
-        <a href='#' class="medium-color" onclick=<?php print $onclick1; ?>><? print translate("sajat_mondatok_10"); ?></a>
-        <a href='#' class="medium-color" onclick=<?php print $onclick4; ?>><? print translate("sajat_mondat_szo"); ?></a>
-        <a class="medium-color show-menu" onclick="audioSzoba();" href="#"><? print translate("audioszoba"); ?></a>
-        <?php
-        if ($userObject && in_array($userObject['status'], array(4, 5, 6))) {
-        ?>
-          <a href='#' class="white-color" onclick="p_Click(event)"><?php print translate('tandijak'); ?></a>
-          <a href='#' class="white-color" onclick="t_Click(event)"><?php print translate('tanulok'); ?></a>
-        <?php }
-        if ($userObject) {
-        ?>
-          <a href='#' class="logout" onclick="event.stopPropagation();location.href='logout.php'"><?php print translate('kijelentkezes'); ?>
-          </a>
+
+            .dropdown-content a:hover {
+              background: #444;
+            }
+
+            .dropdown .medium-color {
+              cursor: pointer;
+            }
+
+            .dropdown-content {
+              box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+            }
+
+            .dropdown .show {
+              display: block !important;
+            }
+          </style>
+          <script>
+            document.addEventListener('click', function(e) {
+              var submenu = document.getElementById('szogyakorlas-submenu');
+              if (submenu && !e.target.closest('.dropdown')) {
+                submenu.classList.remove('show');
+              }
+            });
+          </script>
         <?php } ?>
+        <?php if ($userObject && !in_array($userObject["status"], array(1, 2))) { ?>
+          <a href='#' class="medium-color" onclick=<?php print $onclick1; ?>><? print translate("sajat_mondatok_10"); ?></a>
+          <a href='#' class="medium-color" onclick=<?php print $onclick4; ?>><? print translate("sajat_mondat_szo"); ?></a>
+          <a class="medium-color show-menu" onclick="audioSzoba();" href="#"><? print translate("audioszoba"); ?></a>
+          <?php
+          if ($userObject && in_array($userObject['status'], array(4, 5, 6))) {
+          ?>
+            <a href='#' class="white-color" onclick="p_Click(event)"><?php print translate('tandijak'); ?></a>
+            <a href='#' class="white-color" onclick="t_Click(event)"><?php print translate('tanulok'); ?></a>
+          <?php }
+          if ($userObject) {
+          ?>
+            <a href='#' class="logout" onclick="event.stopPropagation();location.href='logout.php'"><?php print translate('kijelentkezes'); ?>
+            </a>
+          <?php } ?>
+        </span>
+      </div>
+    <?php } ?>
+
+    <?php if ($userObject['status'] == 6) { ?>
+      <div class="submenu admin-menu">
+        <a href='#' class="menu-link" onclick=<?php print $onclick2; ?>><? print translate("kitolto"); ?></a>
+        <a href='#' class="menu-link" onclick="tudastar();"><? print translate("tudastar_title"); ?></a>
+        <a href='#' class="menu-link" onclick="szotarFeltoltes();"><? print translate("feltoltes"); ?></a>
+        <a href='#' class="menu-link" onclick="location.href='main.php?content=wordCategorize&source=welcome'"><? print translate("kategorizalas"); ?></a>
+      </div>
+    <?php }  ?>
+
+    <?php if ($userObject) { ?>
+      <span class="classroom submenu">
+        <a id="aTanuloszoba" href='#' class="button classroom-button" title=<?php print "'" . translate("") . "'" ?> onclick=<?php print $onclick3; ?>><? print translate("tanuloszoba"); ?></a>
       </span>
-    </div>
-  <?php } ?>
+    <?php } ?>
 
-  <?php if ($userObject['status'] == 6) { ?>
-    <div class="submenu admin-menu">
-      <a href='#' class="menu-link" onclick=<?php print $onclick2; ?>><? print translate("kitolto"); ?></a>
-      <a href='#' class="menu-link" onclick="tudastar();"><? print translate("tudastar_title"); ?></a>
-      <a href='#' class="menu-link" onclick="szotarFeltoltes();"><? print translate("feltoltes"); ?></a>
-      <a href='#' class="menu-link" onclick="location.href='main.php?content=wordCategorize&source=welcome'"><? print translate("kategorizalas"); ?></a>
-    </div>
-  <?php }  ?>
-
-  <?php if ($userObject) { ?>
-    <span class="classroom submenu">
-      <a id="aTanuloszoba" href='#' class="button classroom-button" title=<?php print "'" . translate("") . "'" ?> onclick=<?php print $onclick3; ?>><? print translate("tanuloszoba"); ?></a>
-    </span>
-  <?php } ?>
-
-  <!-- <?php if ($userObject) { ?>
+    <!-- <?php if ($userObject) { ?>
   <table width='100%' align='center' valign='center' style=<?php print "'border: 1px solid " . $globalcolor . ";'"; ?> cellpadding='0' cellspacing='0'>
     <tr>
       <td colspan='3' align='right' style=<?php print "'padding-top:5px;padding-right:10px;background:" . $globalcolor . ";'"; ?>><a id="legujjabbszavak" href='#' style='color:white;font-size:12pt;'></a>
@@ -101,22 +109,22 @@
     </tr>
     <tr>
       <?php
-          print "<td valign='top' align='left' style='padding-left:250px;padding-top:10px;padding-bottom:10px;font-size:10pt;'><font color='grey'>";
-          $five_days_ago = strtotime('-5 days');
-          $five_days_formatted = date('Y-m-d', $five_days_ago);
-          if ($userObject["forras_nyelv"] == 0 && $userObject["nyelv"] == 1) {
-            $updated_words = getLastFiveUpdatedWords($five_days_formatted);
-            foreach ($updated_words as $row) {
-              print("<p><b>" . $row["word_angol"] . "</b>&nbsp;-&nbsp;" . $row["word_hun"] . "</p>");
-            }
-          } ?>
+            print "<td valign='top' align='left' style='padding-left:250px;padding-top:10px;padding-bottom:10px;font-size:10pt;'><font color='grey'>";
+            $five_days_ago = strtotime('-5 days');
+            $five_days_formatted = date('Y-m-d', $five_days_ago);
+            if ($userObject["forras_nyelv"] == 0 && $userObject["nyelv"] == 1) {
+              $updated_words = getLastFiveUpdatedWords($five_days_formatted);
+              foreach ($updated_words as $row) {
+                print("<p><b>" . $row["word_angol"] . "</b>&nbsp;-&nbsp;" . $row["word_hun"] . "</p>");
+              }
+            } ?>
       </font>
       </td>
     </tr>
   </table>
 <?php } ?> -->
 
-  <!-- <table>
+    <!-- <table>
  <?php if ($userObject) { ?>
       <tr>
         <td valign='top' align='center' style='padding-top:10'>
@@ -140,4 +148,7 @@
     <?php }  ?> 
 </table> -->
 
-</div>
+  </div>
+</body>
+
+</html>
